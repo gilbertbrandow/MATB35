c0c8cd9904d21ab368c9fc321428f8dd3cef1a01
Gilbert Brandow <simon@0100.se>
2025-11-21 20:24:33 +0100

    Updated problem statements

--- Assignment/main.py
+++ Assignment/main.py
@@ -23,43 +23,43 @@ def main() -> None:
 
 
 def with_repetition_ordered_sentence(n: int, k: int) -> str:
-    problem_statement: str = f"A code of length {k} built from {n} symbols per slot"
+    problem_statement: str = f"A program executes {k} function{'s' if k > 1 else ''} from {n} possible function{'s' if n > 1 else ''}"
     count: int = n ** k
     
-    return f"{problem_statement} has {count} possible code{"s" if count > 1 else ""}."
+    return f"{problem_statement} has {count} possible execution sequence{'s' if count > 1 else ''}."
 
 
 def without_repetition_ordered_sentence(n: int, k: int) -> str:
-    problem_statement: str = f"Asking {k} of {n} people to stand in a line"
+    problem_statement: str = f"Seating {k} out of {n} guests at a wedding"
 
     if k > n:
         return f"{problem_statement} is undefined when k > n."
     
     count: int = factorial(n) // factorial(n - k)
     
-    return f"{problem_statement} can result in {count} different queues."
+    return f"{problem_statement} can result in {count} different seating arrangement{'s' if count > 1 else ''}."
 
 
 def without_repetition_unordered_sentence(n: int, k: int) -> str:
-    problem_statement: str = f"Choosing {k} items from {n} without repetition"
+    problem_statement: str = f"Choosing {k} title{'s' if k > 1 else ''} from a library of {n} book{'s' if n > 1 else ''}"
 
     if k > n:
         return f"{problem_statement} is undefined when k > n."
     
     count: int = factorial(n) // (factorial(n - k) * factorial(k))
     
-    return f"{problem_statement} can be done in {count} way{"s" if count > 1 else ""}."
+    return f"{problem_statement} can be done in {count} way{'s' if count > 1 else ''}."
 
 
 def with_repetition_unordered_sentence(n: int, k: int) -> str:
-    problem_statement: str = f"Distributing {k} identical items among {n} recipients"
+    problem_statement: str = f"Distributing {k} identical item{'s' if k > 1 else ''} among {n} recipient{'s' if n > 1 else ''}"
     
     if n == 0 and k > 0:
         return f"{problem_statement} is undefined when n=0."
     
     count: int = factorial(n + k - 1) // (factorial(n - 1) * factorial(k)) if n > 0 else 1
     
-    return f"{problem_statement} can be done in {count} way{"s" if count > 1 else ""}."
+    return f"{problem_statement} can be done in {count} way{'s' if count > 1 else ''}."
 
 
 def factorial(n: int) -> int: 
